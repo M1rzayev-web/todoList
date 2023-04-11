dd0b568fc60353fd2edb1d2017eb7bf1c40dbe4b
jaloliddimirzayev <jaloliddintuitdif3152gmail.com>
2023-04-11 10:03:33 +0500

    ok

--- src/component/Magazin/Magazin.jsx
+++ src/component/Magazin/Magazin.jsx
@@ -1,29 +1,30 @@
 import React, { useState } from "react";
 import { ArrayPhone } from "./magazinArray";
 import "./magazin.css";
+
 function Magazin() {
   const [count, setCount] = useState({});
+  const [array, setArray] = useState(ArrayPhone);
 
   const inc = (id) => {
     setCount((prevCount) => ({ ...prevCount, [id]: (prevCount[id] || 0) + 1 }));
   };
- const Remove = (id) => {
-   setCount((prevCount) => {
-     const newCount = { ...prevCount };
-     delete newCount[id];
-     return newCount;
-   });
+
+  const handleDelete = (id) => {
+    setArray(array.filter((phone) => phone.id !== id));
+  };
+ const DeleteAll = () => {
+   setArray([]);
  };
 
-
-  const phoneList = ArrayPhone.map((phone) => (
+  const phoneList = array.map((phone) => (
     <article className="Phone-item" key={phone.id}>
       <img src={phone.images} alt={phone.name} />
       <div className="card-div">
         <h3>{phone.name}</h3>
         <span className="price">${phone.price}</span>
         <br />
-        <button onClick={() => Remove(phone.id)}>Remove</button>
+        <button onClick={() => handleDelete(phone.id)}>Remove</button>
       </div>
       <div className="sanovchi">
         <button onClick={() => inc(phone.id)}>+</button>
@@ -36,7 +37,6 @@ function Magazin() {
                 [phone.id]: prevCount[phone.id] - 1,
               }));
             }
-           
           }}
         >
           -
@@ -53,10 +53,10 @@ function Magazin() {
   const totalPrice = Object.keys(count).reduce(
     (total, id) =>
       total +
-      (count[id] || 0) *
-        ArrayPhone.find((phone) => phone.id === Number(id)).price,
+      (count[id] || 0) * array.find((phone) => phone.id === Number(id)).price,
     0
   );
+
   return (
     <div className="Magazin">
       <div className="header">
@@ -68,23 +68,13 @@ function Magazin() {
       </div>
       <div className="main">
         <h3>Your Bag</h3>
-
         {phoneList}
       </div>
       <div className="Narxlar">
         <h3>Total</h3>
         <p className="qiymatPrice">${totalPrice}</p>
       </div>
-      <button
-        className="clearAll"
-        onClick={() => {
-          if (phoneList.length > 0) {
-            return "";
-          }
-        }}
-      >
-        Clear All
-      </button>
+      <button className="clearAll" onClick={DeleteAll}>ClearAll</button>
     </div>
   );
 }
